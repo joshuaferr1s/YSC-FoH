6f1154521e595d4107ac3aab955b1d4dfe2da297
Joshua Ferris <jajf500@york.ac.uk>
2018-01-30 21:56:31 +0000

    Apply fixes from StyleCI

--- file_helpers.py
+++ file_helpers.py
@@ -7,20 +7,24 @@ def load_movie_data_file(file_name):
         dict_from_file = json.load(inf)
     return dict_from_file
 
+
 def write_file(path, data):
     with open(path, 'w') as f:
         for i in data:
             f.write(i + '\n')
 
+
 def write_movie_dict(path, data):
     with open(path, 'w') as f:
         json.dump(data, f, indent=4)
+
 
 def get_files(d):
     movies = list()
     for i in d:
         movies.append(i)
     return movies
+
 
 def rem_dups(raw_list):
     new_list = list()
