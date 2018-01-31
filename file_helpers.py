6411c1e014ae6972f2666acac131aeda8194d994
joshuaferr1s <jajf500@york.ac.uk>
2018-01-31 17:11:37 +0000

    Condensed code

--- file_helpers.py
+++ file_helpers.py
@@ -8,27 +8,13 @@ def load_movie_data_file(file_name):
     return dict_from_file
 
 
-def write_file(path, data):
-    with open(path, 'w') as f:
-        for i in data:
-            f.write(i + '\n')
-
-
 def write_movie_dict(path, data):
     with open(path, 'w') as f:
         json.dump(data, f, indent=4)
 
 
-def get_files(d):
+def keys_to_list(d):
     movies = list()
     for i in d:
         movies.append(i)
     return movies
-
-
-def rem_dups(raw_list):
-    new_list = list()
-    for i in raw_list:
-        if i not in new_list:
-            new_list.append(i)
-    return new_list
